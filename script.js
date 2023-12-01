0da575e75d753ac023aa1e960939393ba6caeb4c
Track404 <111713000+Track404@users.noreply.github.com>
2023-12-01 16:39:45 +0100

    working program with simple UI can create new grid with user input and delete the last one

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
-
+//fucntion that create a new grid if div*div
 
 function CreateGrid(numberOfDiv) {
-    
-    let allDiv = document.querySelector("#allDiv");
+  let allDiv = document.querySelector("#allDiv");
 
-        for (let i = 0; i < numberOfDiv; i++) {
-        
-            let myRow= document.createElement("div");
-            myRow.classList.add("row");
-            allDiv.appendChild(myRow);
+  for (let i = 0; i < numberOfDiv; i++) {
+    let myRow = document.createElement("div");
+    myRow.classList.add("row");
+    allDiv.appendChild(myRow);
 
-            for (let j = 0; j < numberOfDiv; j++){
-            let myPixel = document.createElement("div");
-            myPixel.classList.add("pixel");
-            myRow.appendChild(myPixel);
-
-            }
-        }
-        
-        
-        
-
+    for (let j = 0; j < numberOfDiv; j++) {
+      let myPixel = document.createElement("div");
+      myPixel.classList.add("pixel");
+      myRow.appendChild(myPixel);
+    }
+  }
 }
 
+// function that when you click on a button it create and delete the last grid
 
+function makeNewGrid() {
+  let button = document.querySelector("#theButton");
+  
+  button.addEventListener("click", () => {
+    numberOfDiv = prompt("New Grid Size");
+    deleteDiv();
+    CreateGrid(numberOfDiv);
+    makeColorDiv();
+  });
+}
+
+// function that when you mouse pass a div it change color to black
+
+function makeColorDiv() {
+    let colorChange = document.querySelectorAll(".pixel");
+
+    colorChange.forEach(function (colorChange) {
+      colorChange.addEventListener("mouseover", () => {
+        colorChange.setAttribute("style", "background: black;");
+      });
+    });
+       
+}
+
+//function that delete all the div with the row class
+function deleteDiv() {
+    let allRow = document.querySelectorAll(".row");
+    allRow.forEach((element) => {
+        element.remove();
+      });
+}
 let numberOfDiv = 16;
 
 CreateGrid(numberOfDiv);
-
-let colorChange = document.querySelectorAll(".pixel");
-
-colorChange.forEach(function(colorChange) {
-    colorChange.addEventListener("mouseover", () =>{
-      colorChange.setAttribute('style', 'background: black;');
-    });
-  });
-
-let button = document.querySelector("#theButton");
+makeColorDiv();
+makeNewGrid();
