dfc9ad8b49773f469d0ec34de1067f06b700cdbb
Track404 <111713000+Track404@users.noreply.github.com>
2023-12-02 10:47:55 +0100

    add the ramdom color when hover the divs

--- script.js
+++ script.js
@@ -36,7 +36,8 @@ function makeColorDiv() {
 
   colorChange.forEach(function (colorChange) {
     colorChange.addEventListener("mouseover", () => {
-      colorChange.setAttribute("style", "background: black;");
+        colorChange.style.backgroundColor = generateColor(); 
+
     });
   });
 }
@@ -50,7 +51,20 @@ function deleteDiv() {
   });
 }
 
+// create a random HEX color
+
+function generateColor(){
+    const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+    let code = "";
+    for(let i=0; i<6; i++){
+     code += hexArray[Math.floor(Math.random()*16)];
+    }
+    return `#${code}`
+   }
+   
+
 //initial grid
+
 let numberOfDiv = 16;
 
 CreateGrid(numberOfDiv);
