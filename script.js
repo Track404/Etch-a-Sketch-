336ee99b83eba46d9e51b600129053df67923ef8
Track404 <111713000+Track404@users.noreply.github.com>
2023-12-02 10:18:46 +0100

    UI upgrade

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function CreateGrid(numberOfDiv) {
 
 function makeNewGrid() {
   let button = document.querySelector("#theButton");
-  
+
   button.addEventListener("click", () => {
     numberOfDiv = prompt("New Grid Size");
     deleteDiv();
@@ -32,25 +32,29 @@ function makeNewGrid() {
 // function that when you mouse pass a div it change color to black
 
 function makeColorDiv() {
-    let colorChange = document.querySelectorAll(".pixel");
+  let colorChange = document.querySelectorAll(".pixel");
 
-    colorChange.forEach(function (colorChange) {
-      colorChange.addEventListener("mouseover", () => {
-        colorChange.setAttribute("style", "background: black;");
-      });
+  colorChange.forEach(function (colorChange) {
+    colorChange.addEventListener("mouseover", () => {
+      colorChange.setAttribute("style", "background: black;");
     });
-       
+  });
 }
 
 //function that delete all the div with the row class
+
 function deleteDiv() {
-    let allRow = document.querySelectorAll(".row");
-    allRow.forEach((element) => {
-        element.remove();
-      });
+  let allRow = document.querySelectorAll(".row");
+  allRow.forEach((element) => {
+    element.remove();
+  });
 }
+
+//initial grid
 let numberOfDiv = 16;
 
 CreateGrid(numberOfDiv);
 makeColorDiv();
+
+//new grid buttonk
 makeNewGrid();
